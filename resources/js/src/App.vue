063a6695b06cff4065aa7c787724b66f935d9e86
Mostafa <mostafarezaie.500@gmail.com>
2023-09-05 22:39:07 +0330

    feat: install vue

---
+++ resources/js/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div class="py-24 bg-gray-100">
+      <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+          <input type="text" v-model="value">
+          <div class="p-6 text-gray-900">
+            {{ value }}
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const value = ref('Type here!');
+
+</script>
