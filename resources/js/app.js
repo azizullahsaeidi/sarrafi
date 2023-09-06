d2661ad0abac3b39a4850177f95e730a3f1d3b47
azizullahsaeidi <azizullahsaeidi@gmail.com>
2023-09-06 11:58:54 +0430

    Move down RolespermissionsToVue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,9 @@
 import RolesPermissionsToVue from "../../vendor/geowrgetudor/laravel-spatie-permissions-vue/src/js";
 import './bootstrap';
 
-app.use(RolesPermissionsToVue);
 import { createApp } from "vue";
 import App from './src/App.vue'
 
 const app = createApp(App)
+app.use(RolesPermissionsToVue);
 app.mount('#app')
